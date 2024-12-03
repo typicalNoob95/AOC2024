46507a2d4ddd1b6be66e5daf4de9177e18b1e378
typicalNoob95 <francislajoie1995@gmail.com>
2024-12-03 11:23:27 -0500

    Computed the sample properly.

--- day_two/day_two.py
+++ day_two/day_two.py
@@ -1,10 +1,29 @@
 def get_reports_from_file(filepath):
     with open(filepath) as file:
         lines = file.readlines()
-        return [line.replace("\n", "").split(" ") for line in lines]
+        reports_as_string =  [line.replace("\n", "").split(" ") for line in lines]
+        reports = []
+        for ras in reports_as_string:
+            reports.append(list(map(int, ras)))
+        return reports
 
-def report_is_increasing(report):
-    return report[1] - report[0] > 0
+def levels_are_increasing(actual_level, next_level):
+    return actual_level - next_level < 0
+
+def levels_difference_in_specs(actual_level, next_level):
+    difference_range = range(1,4)
+    return abs(actual_level - next_level) in difference_range
+
+def report_is_safe(report):
+    increasing = levels_are_increasing(report[0], report[1])
+    safe = True
+    for i in range(len(report) - 1):
+        if levels_are_increasing(report[i], report[i + 1]) != increasing or not levels_difference_in_specs(report[i], report[i + 1]):
+            safe = False
+            return safe
+    return safe
 
 if __name__ == "__main__":
     reports = get_reports_from_file("/home/fl/PycharmProjects/AOC2024/day_two/sample.txt")
+    for report in reports:
+        print(report_is_safe(report))
